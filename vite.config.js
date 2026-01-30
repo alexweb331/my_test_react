5416fc5f01db7db6a806d01ee62cce54e5c10182
god <karpukhin.a@rdn.group>
2026-01-30 13:23:44 +0800

    9 commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // для GitHub Pages используйте '/репозиторий/'
+  base: '/my_test_react', // для GitHub Pages используйте '/репозиторий/'
   build: {
     outDir: 'dist',
     sourcemap: false
